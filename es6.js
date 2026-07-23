48b89a816add1ff371827ad598f9eb8f01df219f
NivedithaDevang <niveditha8277@gmail.com>
2026-07-23 17:26:08 +0530

    ES6 practice exercises

--- es6.js
+++ es6.js
@@ -30,3 +30,99 @@ console.log(addition(a, b));
 console.log(square(x));
 console.log(greet());
 
+
+
+var $variable_name12 = 10 
+        /* Global variable can be accessed
+         from anywhere */
+        function Geeks() {
+
+            var $variable_name12 = 100
+            console.log("<br>Inside Geeks() = " + $variable_name12)
+        }
+        console.log("Outside Geeks() = " + $variable_name12)
+        Geeks();
+
+
+        //another example
+        var exam = "ES6";
+        function result(){
+          var exam = "ES5";
+          console.log("Result function exam: " + exam)
+
+        }
+        console.log("Outside result function : " + exam)
+        result();
+
+
+        const pair = {x:30, y:60};
+
+// Updating the x and y values of the object
+pair.x = 10;
+pair.y = 90;
+
+console.log(pair);
+
+//adding another pair
+pair.z = 20;
+console.log(pair);
+
+
+//constants in for..of loop
+const arr = ['hi', 'hello', 'how'];
+for(const e of arr){
+  console.log(e);
+}
+
+
+
+//void keyword is used to evaluate an expression but do not return any value
+const re = x => x * x;
+console.log(void re(5));
+
+console.log(void 10);
+
+
+
+const compareNumbers = (a, b) => 
+    a > b ? "a is greater than b" : "b is greater than a";
+
+console.log(compareNumbers(10, 20)); // Output: b is greater than a
+
+
+
+//decision making statements
+const compare = (a, b) => {
+    if (a > b) {
+        console.log(a+ " is greater than " +b);
+    } else {
+        console.log(b+ " is greater than " +a);
+    }
+};
+
+compare(10, 20);
+
+
+//timeout function
+setTimeout(function() {
+    console.log('Hello, world!');
+}, 3000);
+
+
+console.log("Start");
+
+setTimeout(function() {
+    console.log("Delayed log after 2000 milliseconds");
+}, 6000);
+
+console.log("End");
+
+
+const person = {
+  test(){
+    setTimeout(function() {
+      console.log(this);
+    }, 2000);
+  },
+};
+console.log(person);
