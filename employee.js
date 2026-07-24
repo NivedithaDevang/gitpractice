33a0dd7122a9ab97ccdd103138111f4ea620b21b
NivedithaDevang <niveditha8277@gmail.com>
2026-07-24 17:53:48 +0530

    Mini project

---
+++ employee.js
@@ -0,0 +1,19 @@
+let employees = [
+    {
+        empid : 1,
+        name : "Niveditha",
+        dept : "Developing",
+        salary : 12
+    },
+    {
+        empid : 2,
+        name : "Dainy",
+        dept : "Developing",
+        salary : 15
+    }
+]
+
+//to display employees
+function display(){
+    
+}
