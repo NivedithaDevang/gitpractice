ee5dd4706c8afe9bd2d6f975fc3fcadb8f35d1c1
NivedithaDevang <niveditha8277@gmail.com>
2026-07-27 16:46:35 +0530

    reference for path_module

---
+++ ref.js
@@ -0,0 +1,29 @@
+//add
+const add = (a, b) =>
+{
+    return a + b;
+}
+module.exports.add = add;
+
+//subtract
+const subs = (a, b) =>
+{
+    return a - b;
+}
+module.exports.subs = subs;
+
+//mult
+const mult = (a, b) =>
+{
+    return a * b;
+}
+module.exports.mult = mult;
+
+
+//divide
+const div = (a, b) =>
+{
+    return a / b;
+}
+module.exports.div = div;
+
